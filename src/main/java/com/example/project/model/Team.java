f316661958f7cb270c82ac8af12fc15d066d4e6a
jun6eom <leejun212@naver.com>
2024-08-20 00:04:32 +0900

    오타 수정

--- src/main/java/com/example/project/model/Team.java
+++ src/main/java/com/example/project/model/Team.java
@@ -17,7 +17,6 @@ import java.util.List;
 @Table(name = "tbl_member")
 public class Team {
 
-    //ㅇㅁㄴㅇㄴㅁ
     @Column(name = "TEAM_ID")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
